9c9084949819f55a8d98515a6f1b8016877b332e
leebs0521 <leebs0521@gmail.com>
2024-09-25 16:36:41 +0900

    Init: initial commit

---
+++ backend/src/main/java/org/prgrms/ola/OlaApplication.java
@@ -0,0 +1,13 @@
+package org.prgrms.ola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OlaApplication.class, args);
+  }
+
+}
---
+++ backend/src/test/java/org/prgrms/ola/OlaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.prgrms.ola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
