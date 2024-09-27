bf00cf8414f6224ba9a2b413fb423e5af178b42c
ECON <lgyoon621@naver.com>
2024-09-27 15:12:54 +0900

    feat: ChattingRoomRepository 추가

---
+++ backend/src/main/java/org/prgrms/devconnect/chatting/repository/ChattingRoomRepository.java
@@ -0,0 +1,9 @@
+package org.prgrms.devconnect.chatting.repository;
+
+import org.prgrms.devconnect.chatting.entity.ChattingRoom;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ChattingRoomRepository extends JpaRepository<ChattingRoom, Long> {
+}
