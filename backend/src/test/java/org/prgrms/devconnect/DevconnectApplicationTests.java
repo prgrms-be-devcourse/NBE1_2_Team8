b40466dfd09e9c5c0dcc2282ff0b8f2453c4f9e5
leebs0521 <leebs0521@gmail.com>
2024-09-27 15:14:50 +0900

    init: 테스트 yaml 정의, H2 의존성 추가

--- backend/src/test/java/org/prgrms/devconnect/DevconnectApplicationTests.java
+++ backend/src/test/java/org/prgrms/devconnect/DevconnectApplicationTests.java
@@ -2,8 +2,10 @@ package org.prgrms.devconnect;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DevconnectApplicationTests {
 
   @Test
