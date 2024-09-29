04d3c6c3709803567d57875f3ee3759b9fb3659f
JeongIna <jeongina89@gmail.com>
2024-09-29 19:56:18 +0900

    fix: @RequiredArgsConstructor 추가,반환형 Void로 수정

--- backend/src/main/java/org/prgrms/devconnect/api/controller/board/BoardController.java
+++ backend/src/main/java/org/prgrms/devconnect/api/controller/board/BoardController.java
@@ -1,9 +1,9 @@
 package org.prgrms.devconnect.api.controller.board;
 
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.prgrms.devconnect.api.controller.board.dto.request.BoardCreateRequestDto;
 import org.prgrms.devconnect.api.service.board.BoardService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,12 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/boards")
+@RequiredArgsConstructor
 public class BoardController {
-  @Autowired
-  private BoardService boardService;
+
+  private final BoardService boardService;
 
   @PostMapping
-  public ResponseEntity<Long> createBoard(@RequestBody @Valid BoardCreateRequestDto boardCreateRequestDto) {
+  public ResponseEntity<Void> createBoard(@RequestBody @Valid BoardCreateRequestDto boardCreateRequestDto) {
     boardService.createBoard(boardCreateRequestDto);
     return ResponseEntity.status(HttpStatus.CREATED).build();
   }
