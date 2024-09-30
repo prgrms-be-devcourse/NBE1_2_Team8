a632908d2417a6fd861a52f76416644d270b233c
ECON <lgyoon621@naver.com>
2024-09-30 15:41:48 +0900

    refactor: @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class) 적용

--- backend/src/main/java/org/prgrms/devconnect/api/controller/chatting/dto/request/ChatRoomRequest.java
+++ backend/src/main/java/org/prgrms/devconnect/api/controller/chatting/dto/request/ChatRoomRequest.java
@@ -1,9 +1,12 @@
 package org.prgrms.devconnect.api.controller.chatting.dto.request;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import jakarta.validation.constraints.NotNull;
 
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record ChatRoomRequest(
         @NotNull
-        Long receiver_id
+        Long receiverId
 ) {
 }
