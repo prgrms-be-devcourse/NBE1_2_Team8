9ea621c49c46748a1b2d48187a219896c57d23ec
leebs0521 <leebs0521@gmail.com>
2024-09-30 14:32:03 +0900

    test: TechStackQueryService 기능 테스트 추가

--- backend/src/test/java/org/prgrms/devconnect/api/service/techstack/TechStackQueryServiceTest.java
+++ backend/src/test/java/org/prgrms/devconnect/api/service/techstack/TechStackQueryServiceTest.java
@@ -5,6 +5,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
+import java.util.Optional;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -27,9 +28,40 @@ class TechStackQueryServiceTest {
   @InjectMocks
   private TechStackQueryService techStackQueryService;
 
-  @DisplayName("유효한_아아디_리스트가_주어지면_기술스택_리스트를_반환한다")
+  @DisplayName("유효한_아이디가_주어지면_기술스택을_반환한다")
   @Test
-  void 유효한_아아디_리스트가_주어지면_기술스택_리스트를_반환한다() {
+  void 유효한_아이디가_주어지면_기술스택을_반환한다() {
+    // given
+    Long techStackId = 1L;
+    when(techStackRepository.findById(techStackId))
+        .thenReturn(Optional.ofNullable(TechStackFixture.createTechStack()));
+
+    // when
+    TechStack techStack = techStackQueryService.getTechStackByIdOrThrow(techStackId);
+
+    // then
+    assertThat(techStack).isNotNull();
+    verify(techStackRepository).findById((techStackId));
+  }
+
+  @DisplayName("유효하지않은_아이디가_주어지면_에러가_발생한다")
+  @Test
+  void 유효하지않은_아이디가_주어지면_에러가_발생한다() {
+    // given
+    Long techStackId = 100L;
+    when(techStackRepository.findById(techStackId))
+        .thenReturn(Optional.empty());
+
+    // when // then
+    Assertions.assertThatThrownBy(
+            () -> techStackQueryService.getTechStackByIdOrThrow(techStackId)
+        ).isInstanceOf(TechStackException.class)
+        .hasMessage(ExceptionCode.NOT_FOUND_TECH_STACK.getMessage());
+  }
+
+  @DisplayName("유효한_아이디_리스트가_주어지면_기술스택_리스트를_반환한다")
+  @Test
+  void 유효한_아이디_리스트가_주어지면_기술스택_리스트를_반환한다() {
     // given
     List<Long> techStackIds = List.of(1L, 2L);
     when(techStackRepository.findAllByTechStackIdIn(techStackIds))
@@ -45,9 +77,9 @@ class TechStackQueryServiceTest {
     verify(techStackRepository).findAllByTechStackIdIn(techStackIds);
   }
 
-  @DisplayName("유효하지않는_아아디_리스트가_주어지면_기술스택_리스트를_반환한다")
+  @DisplayName("유효하지않는_아이디_리스트가_주어지면_에러가_발생한다")
   @Test
-  void 유효하지않는_아아디_리스트가_주어지면_기술스택_리스트를_반환한다() {
+  void 유효하지않는_아이디_리스트가_주어지면_에러가_발생한다() {
     List<Long> techStackIds = List.of(1L, 2L, 100L);
     when(techStackRepository.findAllByTechStackIdIn(techStackIds))
         .thenReturn(
