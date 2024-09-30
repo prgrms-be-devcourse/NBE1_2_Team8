14236c52dd11b3fb4d83dd556c3e7eaf449c9f66
JeongIna <jeongina89@gmail.com>
2024-10-01 01:09:30 +0900

    fix: 게시판 자동마감, 수동마감 로직 추가,Controller 오타 수정

--- backend/src/main/java/org/prgrms/devconnect/api/controller/board/BoardController.java
+++ backend/src/main/java/org/prgrms/devconnect/api/controller/board/BoardController.java
@@ -15,18 +15,18 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class BoardController {
 
-  private final BoardCommandService boardCommendService;
+  private final BoardCommandService boardCommandService;
   private final BoardQueryService boardQueryService;
 
   @PostMapping
   public ResponseEntity<Void> createBoard(@RequestBody @Valid BoardCreateRequestDto boardCreateRequestDto) {
-    boardCommendService.createBoard(boardCreateRequestDto);
+    boardCommandService.createBoard(boardCreateRequestDto);
     return ResponseEntity.status(HttpStatus.CREATED).build();
   }
 
   @DeleteMapping("/{boardId}")
   public ResponseEntity<Void> deleteBoard(@PathVariable Long boardId) {
-    boardCommendService.deleteBoard(boardId);
+    boardCommandService.deleteBoard(boardId);
     return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
   }
 
@@ -34,7 +34,16 @@ public class BoardController {
   public ResponseEntity<Void> updateBoard(
           @PathVariable Long boardId,
           @RequestBody @Valid BoardUpdateRequestDto boardUpdateRequestDto) {
-    boardCommendService.updateBoard(boardId,boardUpdateRequestDto);
+    boardCommandService.updateBoard(boardId,boardUpdateRequestDto);
     return ResponseEntity.status(HttpStatus.OK).build();
   }
+
+  @PutMapping("/{boardId}/close")
+  public ResponseEntity<Void> closeBoard(
+          @PathVariable Long boardId) {
+    boardCommandService.closeBoardManually(boardId);
+    return ResponseEntity.status(HttpStatus.OK).build();
+  }
+
+
 }
