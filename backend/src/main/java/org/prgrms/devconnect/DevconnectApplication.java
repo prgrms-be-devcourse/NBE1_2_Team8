32777c2f6de24a51114527334fa2e066d00ad490
eundeang <111877048+eundeang@users.noreply.github.com>
2024-09-30 16:34:12 +0900

    feat: aop 설정을 위한 @EnableAspectJAutoProxy 추가

--- backend/src/main/java/org/prgrms/devconnect/DevconnectApplication.java
+++ backend/src/main/java/org/prgrms/devconnect/DevconnectApplication.java
@@ -2,10 +2,12 @@ package org.prgrms.devconnect;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableAspectJAutoProxy
 public class DevconnectApplication {
 
   public static void main(String[] args) {
