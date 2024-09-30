b7fedf77c397fcc53c3f152cea3efe6a76a1de85
ECON <lgyoon621@naver.com>
2024-09-30 15:32:35 +0900

    remove: JobPostRepository.java 이상한 경로에 있던 중복 파일 제거

--- backend/src/main/java/org/prgrms/devconnect/domain/define/jobpost/entity/repository/JobPostRepository.java
+++
@@ -1,7 +0,0 @@
-package org.prgrms.devconnect.domain.define.jobpost.entity.repository;
-
-import org.prgrms.devconnect.domain.define.jobpost.entity.JobPost;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JobPostRepository extends JpaRepository<JobPost, Long> {
-}
