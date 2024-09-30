c0e50ec2d4179d8c4edf84349eb3794eaa1e10de
JeongIna <jeongina89@gmail.com>
2024-10-01 01:11:03 +0900

    fix: 상태가 모집중이면서 마감일이 지난 모집글을 조회하는 로직 추가

--- backend/src/main/java/org/prgrms/devconnect/api/service/board/BoardQueryService.java
+++ backend/src/main/java/org/prgrms/devconnect/api/service/board/BoardQueryService.java
@@ -4,9 +4,13 @@ import lombok.RequiredArgsConstructor;
 import org.prgrms.devconnect.common.exception.ExceptionCode;
 import org.prgrms.devconnect.common.exception.board.BoardException;
 import org.prgrms.devconnect.domain.define.board.entity.Board;
+import org.prgrms.devconnect.domain.define.board.entity.constant.BoardStatus;
 import org.prgrms.devconnect.domain.define.board.repository.BoardRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -18,6 +22,10 @@ public class BoardQueryService {
   public Board getBoardByIdOrThrow(Long boardId) {
     return boardRepository.findById(boardId)
             .orElseThrow(() -> new BoardException(ExceptionCode.NOT_FOUND_BOARD));
+  }
+
+  public List<Board> findAllByEndDateAndStatus() {
+    return boardRepository.findAllByEndDateAndStatus(LocalDateTime.now(), BoardStatus.RECRUITING);
   }
 }
 
