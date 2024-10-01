158e21f77fee2f497216210e14c3ae040397e4fe
ECON <lgyoon621@naver.com>
2024-10-01 15:21:45 +0900

    feat: DTO 추가 및 수정

--- backend/src/main/java/org/prgrms/devconnect/api/controller/chatting/dto/response/ChatRoomListResponse.java
+++ backend/src/main/java/org/prgrms/devconnect/api/controller/chatting/dto/response/ChatRoomListResponse.java
@@ -6,7 +6,9 @@ import org.prgrms.devconnect.domain.define.chatting.entity.constant.ChattingRoom
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record ChatRoomListResponse(
-        Long memberId,
+        Long memberId, //TODO 닉네임 추가 필요
+        // memberId, nickname {Long chatpartId, Long roomId, ChattingRoomStatus status ... 같은 응답형태로 변경필요}
+        Long chatpartId,
         Long roomId,
         ChattingRoomStatus status
 ) {
---
+++ backend/src/main/java/org/prgrms/devconnect/api/controller/chatting/dto/response/MessageResponse.java
@@ -0,0 +1,15 @@
+package org.prgrms.devconnect.api.controller.chatting.dto.response;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import java.time.LocalDateTime;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public record MessageResponse(
+        Long messageId,
+        Long senderId,
+        String content,
+        LocalDateTime createdAt
+) {
+}
