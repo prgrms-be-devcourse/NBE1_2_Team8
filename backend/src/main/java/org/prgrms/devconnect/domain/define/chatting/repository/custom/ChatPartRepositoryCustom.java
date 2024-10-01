bd61aa2edfbae195e0bb160da087c0dd536d32d7
ECON <lgyoon621@naver.com>
2024-10-01 15:10:11 +0900

    feat: 메세지 조회하는 쿼리 메서드 추가 및 응답 형식 변경

--- backend/src/main/java/org/prgrms/devconnect/domain/define/chatting/repository/custom/ChatPartRepositoryCustom.java
+++ backend/src/main/java/org/prgrms/devconnect/domain/define/chatting/repository/custom/ChatPartRepositoryCustom.java
@@ -1,6 +1,7 @@
 package org.prgrms.devconnect.domain.define.chatting.repository.custom;
 
 import org.prgrms.devconnect.api.controller.chatting.dto.response.ChatRoomListResponse;
+import org.prgrms.devconnect.api.controller.chatting.dto.response.MessageResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -11,4 +12,6 @@ public interface ChatPartRepositoryCustom {
   List<ChatRoomListResponse> findByMemberIdAndStatusIsActive(Long memberId);
 
   Page<ChatRoomListResponse> findByMemberIdAndStatusIsActiveWithPage(Long memberId, Pageable pageable);
+
+  Page<MessageResponse> findAllMessageByRoomId(Long roomId, Pageable pageable);
 }
