8939e69caf5c48ccc48a1f9c5ac97da2de388c8f
ECON <lgyoon621@naver.com>
2024-10-01 15:10:11 +0900

    feat: 메세지 조회하는 쿼리 메서드 추가 및 응답 형식 변경

--- backend/src/main/java/org/prgrms/devconnect/domain/define/chatting/repository/custom/ChatPartRepositoryCustomImpl.java
+++ backend/src/main/java/org/prgrms/devconnect/domain/define/chatting/repository/custom/ChatPartRepositoryCustomImpl.java
@@ -4,8 +4,10 @@ import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.prgrms.devconnect.api.controller.chatting.dto.response.ChatRoomListResponse;
+import org.prgrms.devconnect.api.controller.chatting.dto.response.MessageResponse;
 import org.prgrms.devconnect.domain.define.chatting.entity.QChatParticipation;
 import org.prgrms.devconnect.domain.define.chatting.entity.QChattingRoom;
+import org.prgrms.devconnect.domain.define.chatting.entity.QMessage;
 import org.prgrms.devconnect.domain.define.chatting.entity.constant.ChattingRoomStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -17,14 +19,16 @@ import java.util.List;
 public class ChatPartRepositoryCustomImpl implements ChatPartRepositoryCustom {
 
   private final JPAQueryFactory queryFactory;
+
   @Override
   public List<ChatRoomListResponse> findByMemberIdAndStatusIsActive(Long memberId) {
     QChatParticipation chatpart = QChatParticipation.chatParticipation;
     QChattingRoom chattingRoom = QChattingRoom.chattingRoom;
 
     return queryFactory.select(Projections.constructor(
-                    ChatRoomListResponse.class ,
+                    ChatRoomListResponse.class,
                     chatpart.member.memberId,
+                    chatpart.chatPartId,
                     chattingRoom.roomId,
                     chattingRoom.status
             ))
@@ -42,6 +46,7 @@ public class ChatPartRepositoryCustomImpl implements ChatPartRepositoryCustom {
     List<ChatRoomListResponse> content = queryFactory.select(Projections.constructor(
                     ChatRoomListResponse.class,
                     chatpart.member.memberId,
+                    chatpart.chatPartId,
                     chattingRoom.roomId,
                     chattingRoom.status
             ))
@@ -54,13 +59,42 @@ public class ChatPartRepositoryCustomImpl implements ChatPartRepositoryCustom {
             .orderBy()
             .fetch();
 
+    // 전체 레코드 수 계산
     long total = queryFactory.select(chattingRoom.count())
-            .from(chatpart)
+            .from(chattingRoom)
             .join(chatpart.chattingRoom, chattingRoom)
             .where(chatpart.member.memberId.eq(memberId)
                     .and(chattingRoom.status.eq(ChattingRoomStatus.ACTIVE)))
             .fetchOne();
 
     return new PageImpl<>(content, pageable, total);
+  }
+
+  @Override
+  public Page<MessageResponse> findAllMessageByRoomId(Long roomId, Pageable pageable) {
+    QChatParticipation chatpart = QChatParticipation.chatParticipation;
+    QMessage message = QMessage.message;
+
+    List<MessageResponse> messages = queryFactory.select(Projections.constructor(MessageResponse.class,
+                    message.messageId,
+                    chatpart.member.memberId,
+                    message.content,
+                    message.createdAt))
+            .from(message)
+            .join(chatpart.messages, message)
+            .where(chatpart.chattingRoom.roomId.eq(roomId))
+            .orderBy(message.createdAt.desc())
+            .offset(pageable.getOffset())
+            .limit(pageable.getPageSize())
+            .fetch();
+
+    // 전체 레코드 수 계산
+    long total = queryFactory.select(message.count())
+            .from(message)
+            .join(chatpart.messages, message)
+            .where(chatpart.chattingRoom.roomId.eq(roomId))
+            .fetchOne();
+
+    return new PageImpl<>(messages, pageable, total);
   }
 }
