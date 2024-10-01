9cf92fbdd44014b8bf22503a3a0a020f652f9fe4
eundeang <111877048+eundeang@users.noreply.github.com>
2024-10-01 09:51:53 +0900

    refact: CQRS 적용을 위해 -CommandService로 이름 변경

--- backend/src/main/java/org/prgrms/devconnect/api/service/alarm/AlarmCommandService.java
+++ backend/src/main/java/org/prgrms/devconnect/api/service/alarm/AlarmCommandService.java
@@ -10,7 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class AlarmService {
+public class AlarmCommandService {
 
   private final AlarmRepository alarmRepository;
 
