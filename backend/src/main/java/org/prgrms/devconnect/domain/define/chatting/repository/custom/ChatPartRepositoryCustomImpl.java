3e0eeda0a09b214fc3d089bd97cd2e3c43ac4677
ECON <lgyoon621@naver.com>
2024-10-01 09:28:31 +0900

    feat: 채팅방 목록 조회 페이지 기능 추가

--- backend/src/main/java/org/prgrms/devconnect/domain/define/chatting/repository/custom/ChatPartRepositoryCustomImpl.java
+++ backend/src/main/java/org/prgrms/devconnect/domain/define/chatting/repository/custom/ChatPartRepositoryCustomImpl.java
@@ -7,6 +7,9 @@ import org.prgrms.devconnect.api.controller.chatting.dto.response.ChatRoomListRe
 import org.prgrms.devconnect.domain.define.chatting.entity.QChatParticipation;
 import org.prgrms.devconnect.domain.define.chatting.entity.QChattingRoom;
 import org.prgrms.devconnect.domain.define.chatting.entity.constant.ChattingRoomStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -29,5 +32,35 @@ public class ChatPartRepositoryCustomImpl implements ChatPartRepositoryCustom {
             .join(chatpart.chattingRoom, chattingRoom)
             .where(chatpart.member.memberId.eq(memberId).and(chattingRoom.status.eq(ChattingRoomStatus.ACTIVE)))
             .fetch();
+  }
+
+  @Override
+  public Page<ChatRoomListResponse> findByMemberIdAndStatusIsActiveWithPage(Long memberId, Pageable pageable) {
+    QChatParticipation chatpart = QChatParticipation.chatParticipation;
+    QChattingRoom chattingRoom = QChattingRoom.chattingRoom;
+
+    List<ChatRoomListResponse> content = queryFactory.select(Projections.constructor(
+                    ChatRoomListResponse.class,
+                    chatpart.member.memberId,
+                    chattingRoom.roomId,
+                    chattingRoom.status
+            ))
+            .from(chatpart)
+            .join(chatpart.chattingRoom, chattingRoom)
+            .where(chatpart.member.memberId.eq(memberId)
+                    .and(chattingRoom.status.eq(ChattingRoomStatus.ACTIVE)))
+            .offset(pageable.getOffset())
+            .limit(pageable.getPageSize())
+            .orderBy()
+            .fetch();
+
+    long total = queryFactory.select(chattingRoom.count())
+            .from(chatpart)
+            .join(chatpart.chattingRoom, chattingRoom)
+            .where(chatpart.member.memberId.eq(memberId)
+                    .and(chattingRoom.status.eq(ChattingRoomStatus.ACTIVE)))
+            .fetchOne();
+
+    return new PageImpl<>(content, pageable, total);
   }
 }
