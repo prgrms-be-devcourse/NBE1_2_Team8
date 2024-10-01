2a96f56d99307ee663e42aebeb1b3e368a33c604
eundeang <111877048+eundeang@users.noreply.github.com>
2024-10-01 09:52:33 +0900

    feat: AlarmQueryService에 전체 조회 메서드 추가

---
+++ backend/src/main/java/org/prgrms/devconnect/api/service/alarm/AlarmQueryService.java
@@ -0,0 +1,29 @@
+package org.prgrms.devconnect.api.service.alarm;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.prgrms.devconnect.api.controller.alarm.response.AlarmsGetResponse;
+import org.prgrms.devconnect.api.service.member.MemberQueryService;
+import org.prgrms.devconnect.domain.define.alarm.entity.Alarm;
+import org.prgrms.devconnect.domain.define.alarm.repository.AlarmRepository;
+import org.prgrms.devconnect.domain.define.member.entity.Member;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class AlarmQueryService {
+
+  private final AlarmRepository alarmRepository;
+
+  private final MemberQueryService memberQueryService;
+
+  public AlarmsGetResponse getAlarmsByMemberId(Long memberId) {
+
+    Member member = memberQueryService.getMemberByIdOrThrow(memberId);
+    List<Alarm> alarms = alarmRepository.findAllByMember(member);
+
+    return AlarmsGetResponse.from(alarms);
+  }
+}
