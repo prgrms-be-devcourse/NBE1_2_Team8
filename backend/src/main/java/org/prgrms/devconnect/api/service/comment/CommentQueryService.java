42077db0cd9506aa7e83dcc326ac6b9a9aec6de2
JeongIna <jeongina89@gmail.com>
2024-10-03 01:38:09 +0900

    feat: 게시글 ID로 댓글을 페이징 처리하여 조회하는 기능 구현

--- backend/src/main/java/org/prgrms/devconnect/api/service/comment/CommentQueryService.java
+++ backend/src/main/java/org/prgrms/devconnect/api/service/comment/CommentQueryService.java
@@ -5,8 +5,11 @@ import org.prgrms.devconnect.common.exception.ExceptionCode;
 import org.prgrms.devconnect.common.exception.comment.CommentException;
 import org.prgrms.devconnect.domain.define.board.entity.Comment;
 import org.prgrms.devconnect.domain.define.board.repository.CommentRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 
 @Service
 @RequiredArgsConstructor
@@ -17,5 +20,9 @@ public class CommentQueryService {
   public Comment getCommentByIdOrThrow(Long commentId) {
     return commentRepository.findById(commentId)
             .orElseThrow(() -> new CommentException(ExceptionCode.NOT_FOUND_COMMENT));
+  }
+
+  public Page<Comment> findAllByBoardId(Long boardId, Pageable pageable){
+    return commentRepository.findAllByBoardId(boardId,pageable);
   }
 }
