1e9d9963fc9fdccefa68c2c73c2f64d97583ed0e
JeongIna <jeongina89@gmail.com>
2024-10-03 01:39:11 +0900

    feat: 게시글별 댓글 페이징 조회 API 추가

--- backend/src/main/java/org/prgrms/devconnect/api/controller/comment/CommentController.java
+++ backend/src/main/java/org/prgrms/devconnect/api/controller/comment/CommentController.java
@@ -2,15 +2,16 @@ package org.prgrms.devconnect.api.controller.comment;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.prgrms.devconnect.api.controller.comment.dto.CommentCreateRequestDto;
+import org.prgrms.devconnect.api.controller.comment.dto.request.CommentCreateRequestDto;
+import org.prgrms.devconnect.api.controller.comment.dto.response.CommentResponseDto;
 import org.prgrms.devconnect.api.service.comment.CommentCommandService;
 import org.prgrms.devconnect.api.service.comment.CommentQueryService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/api/v1/comments")
@@ -23,5 +24,11 @@ public class CommentController {
   public ResponseEntity<Void> createComment(@RequestBody @Valid CommentCreateRequestDto commentCreateRequestDto) {
     commentCommandService.createComment(commentCreateRequestDto);
     return ResponseEntity.status(HttpStatus.CREATED).build();
+  }
+
+  @GetMapping("/{boardId}")
+  public ResponseEntity<Page<CommentResponseDto>> getComments(@PathVariable Long boardId, Pageable pageable) {
+    Page<CommentResponseDto>comments= commentCommandService.getCommentsByBoardId(boardId,pageable);
+    return ResponseEntity.ok(comments);
   }
 }
