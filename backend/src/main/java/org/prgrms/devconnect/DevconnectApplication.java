630dc52a013aa728161c32cb6a3cd9c2c04e3d41
j-ra1n <j_ra1n@naver.com>
2024-10-02 17:31:12 +0900

    feat: Runner 클래스 추가

--- backend/src/main/java/org/prgrms/devconnect/DevconnectApplication.java
+++ backend/src/main/java/org/prgrms/devconnect/DevconnectApplication.java
@@ -3,9 +3,11 @@ package org.prgrms.devconnect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableScheduling // 스케줄러 활성화
 public class DevconnectApplication {
 
   public static void main(String[] args) {
