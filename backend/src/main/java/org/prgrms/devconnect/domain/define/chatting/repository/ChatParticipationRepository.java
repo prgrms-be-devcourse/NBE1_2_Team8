b0eb5351cf5e0a275321092359ff6466a26c191a
ECON <lgyoon621@naver.com>
2024-10-02 11:28:43 +0900

    feat: 채팅참여 Repository에 memberId, chatpartId로 조회하는 메서드 추가

--- backend/src/main/java/org/prgrms/devconnect/domain/define/chatting/repository/ChatParticipationRepository.java
+++ backend/src/main/java/org/prgrms/devconnect/domain/define/chatting/repository/ChatParticipationRepository.java
@@ -7,9 +7,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ChatParticipationRepository extends JpaRepository<ChatParticipation, Long>, ChatPartRepositoryCustom {
   List<ChatParticipation> findAllByChattingRoom_RoomId(Long roomId);
+
+  Optional<ChatParticipation> findByMember_MemberIdAndChattingRoom_RoomId(Long memberId, Long chatRoomId);
 
 }
