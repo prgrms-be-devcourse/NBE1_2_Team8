320d7d245f894b07a2397442927aeff51ffd0771
ECON <lgyoon621@naver.com>
2024-10-02 09:40:23 +0900

    refactor: @NotNull 메세지 추가

--- backend/src/main/java/org/prgrms/devconnect/api/controller/chatting/dto/request/MessageRequest.java
+++ backend/src/main/java/org/prgrms/devconnect/api/controller/chatting/dto/request/MessageRequest.java
@@ -7,7 +7,7 @@ import jakarta.validation.constraints.NotNull;
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record MessageRequest(
-        @NotNull(message = "")
+        @NotNull(message = "채팅 참여 ID는 필수입니다")
         Long chatpartId,
         @NotBlank(message = "메세지는 공백이거나 빈값이면 안됩니다")
         String content
