083ce68696b0bada1d3fb344db911c4f84cb72ce
j-ra1n <j_ra1n@naver.com>
2024-10-02 17:29:52 +0900

    feat: 엔티티 수정 + Repository 추가

--- backend/src/main/java/org/prgrms/devconnect/domain/define/jobpost/entity/JobPostTechStackMapping.java
+++ backend/src/main/java/org/prgrms/devconnect/domain/define/jobpost/entity/JobPostTechStackMapping.java
@@ -1,15 +1,8 @@
 package org.prgrms.devconnect.domain.define.jobpost.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.prgrms.devconnect.domain.define.techstack.entity.TechStack;
@@ -29,8 +22,13 @@ public class JobPostTechStackMapping {
   @JoinColumn(name = "job_post_id", nullable = false)
   private JobPost jobPost;
 
-  @ManyToOne(fetch = FetchType.LAZY)
+  @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
   @JoinColumn(name = "tech_stack_id", nullable = false)
   private TechStack techStack;
 
+  @Builder
+  public JobPostTechStackMapping(JobPost jobPost, TechStack techStack) {
+    this.jobPost = jobPost;
+    this.techStack = techStack;
+  }
 }
