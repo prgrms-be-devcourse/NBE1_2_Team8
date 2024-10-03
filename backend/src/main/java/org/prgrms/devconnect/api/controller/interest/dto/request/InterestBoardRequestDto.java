5578f09ed3ca5ef9cd099d2e6a2107ff66350bf0
leebs0521 <leebs0521@gmail.com>
2024-10-03 19:17:22 +0900

    fix: dto snake_case 적용

--- backend/src/main/java/org/prgrms/devconnect/api/controller/interest/dto/request/InterestBoardRequestDto.java
+++ backend/src/main/java/org/prgrms/devconnect/api/controller/interest/dto/request/InterestBoardRequestDto.java
@@ -1,11 +1,14 @@
 package org.prgrms.devconnect.api.controller.interest.dto.request;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Builder;
 import org.prgrms.devconnect.domain.define.board.entity.Board;
 import org.prgrms.devconnect.domain.define.interest.entity.InterestBoard;
 import org.prgrms.devconnect.domain.define.member.entity.Member;
 
 @Builder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record InterestBoardRequestDto(
     Long memberId,
     Long boardId
