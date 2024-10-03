b109c7c417d472b05714deb6220e97aa9584ff69
eundeang <111877048+eundeang@users.noreply.github.com>
2024-10-03 16:01:58 +0900

    test: 알림 단일 삭제 테스트 코드 추가
    @MockBean으로 선언된 의존관계 발생한 경우에는 application context사용을 위해 @Autowired로 빈 등록을 해줘야 하여 의존성 주입 방식 변경

--- backend/src/test/java/org/prgrms/devconnect/api/service/alarm/AlarmCommandServiceTest.java
+++ backend/src/test/java/org/prgrms/devconnect/api/service/alarm/AlarmCommandServiceTest.java
@@ -1,17 +1,25 @@
 package org.prgrms.devconnect.api.service.alarm;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.prgrms.devconnect.api.controller.member.dto.request.MemberCreateRequestDto;
 import org.prgrms.devconnect.api.service.member.MemberCommandService;
+import org.prgrms.devconnect.common.exception.alarm.AlarmException;
+import org.prgrms.devconnect.domain.define.alarm.entity.Alarm;
+import org.prgrms.devconnect.domain.define.alarm.repository.AlarmRepository;
 import org.prgrms.devconnect.domain.define.member.entity.Member;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
@@ -19,11 +27,15 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 @SpringBootTest
 class AlarmCommandServiceTest {
 
-  @MockBean
-  private AlarmCommandService alarmCommandService;
 
   @MockBean
   private MemberCommandService memberCommandService;  // MockBean으로 변경
+
+  @MockBean
+  private AlarmRepository alarmRepository;
+
+  @Autowired
+  private AlarmCommandService alarmCommandService;
 
 
   @Test
@@ -38,6 +50,30 @@ class AlarmCommandServiceTest {
 
 
     verify(alarmCommandService, times(1)).createWelcomeAlarmWhenSignIn(member);
+  }
+
+  @Test
+  @DisplayName("알림 단일 삭제")
+  void deleteAlarm() {
+    Alarm alarm = mock(Alarm.class);
+
+    when(alarm.getAlarmId()).thenReturn(1L);
+    doNothing().when(alarmRepository).deleteById(any(Long.class));
+
+    alarmCommandService.deleteAlarmByAlarmId(alarm.getAlarmId());
+
+    verify(alarmRepository, times(1)).deleteById(any());
+  }
+
+  @Test
+  @DisplayName("알림 단일 삭제시 존재하지 않은 알림의 예외 처리 테스트")
+  void deleteAlarmWhenNotExistAlarmThrowException() {
+    doThrow(new IllegalArgumentException()).when(alarmRepository).deleteById(any(Long.class));
+
+    Assertions.assertThatThrownBy(() -> alarmCommandService.deleteAlarmByAlarmId(any(Long.class)))
+            .isInstanceOf(AlarmException.class);
+
+    verify(alarmRepository, times(0)).delete(any(Alarm.class));
   }
 
 }
