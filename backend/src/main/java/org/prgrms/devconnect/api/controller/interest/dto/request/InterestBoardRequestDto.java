35ff76f59a0721e21e8ccb0e713ddbd249a3c701
leebs0521 <leebs0521@gmail.com>
2024-10-03 19:07:23 +0900

    feat: dto builder 추가

--- backend/src/main/java/org/prgrms/devconnect/api/controller/interest/dto/request/InterestBoardRequestDto.java
+++ backend/src/main/java/org/prgrms/devconnect/api/controller/interest/dto/request/InterestBoardRequestDto.java
@@ -1,9 +1,11 @@
 package org.prgrms.devconnect.api.controller.interest.dto.request;
 
+import lombok.Builder;
 import org.prgrms.devconnect.domain.define.board.entity.Board;
 import org.prgrms.devconnect.domain.define.interest.entity.InterestBoard;
 import org.prgrms.devconnect.domain.define.member.entity.Member;
 
+@Builder
 public record InterestBoardRequestDto(
     Long memberId,
     Long boardId
