b62e141adefe5964905f53979b765d6eb0a4577e
ECON <lgyoon621@naver.com>
2024-10-04 15:29:16 +0900

    feat: dto Swagger 적용

--- backend/src/main/java/org/prgrms/devconnect/api/controller/chatting/dto/response/ChatRoomListResponse.java
+++ backend/src/main/java/org/prgrms/devconnect/api/controller/chatting/dto/response/ChatRoomListResponse.java
@@ -2,14 +2,21 @@ package org.prgrms.devconnect.api.controller.chatting.dto.response;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.prgrms.devconnect.domain.define.chatting.entity.constant.ChattingRoomStatus;
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record ChatRoomListResponse(
-        Long memberId, //TODO 닉네임 추가 필요
-        // memberId, nickname {Long chatpartId, Long roomId, ChattingRoomStatus status ... 같은 응답형태로 변경필요}
+        @Schema(description = "멤버 ID", example = "2")
+        Long memberId,
+
+        @Schema(description = "채팅 참여자 ID", example = "1")
         Long chatpartId,
+
+        @Schema(description = "채팅 방 ID", example = "100")
         Long roomId,
+
+        @Schema(description = "채팅 방 상태", example = "ACTIVE")
         ChattingRoomStatus status
 ) {
 }
