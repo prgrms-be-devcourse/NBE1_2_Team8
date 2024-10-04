9e31815a1bfc9dbd8e5261578ca5c444bb9051f4
eundeang <111877048+eundeang@users.noreply.github.com>
2024-10-04 15:26:27 +0900

    feat: 전체알림 삭제 메서드 추가

--- backend/src/main/java/org/prgrms/devconnect/domain/define/alarm/repository/AlarmRepository.java
+++ backend/src/main/java/org/prgrms/devconnect/domain/define/alarm/repository/AlarmRepository.java
@@ -8,4 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AlarmRepository extends JpaRepository<Alarm, Long> {
   List<Alarm> findAllByMember(Member member);
+  void deleteAllByMemberMemberId(Long memberId);
 }
