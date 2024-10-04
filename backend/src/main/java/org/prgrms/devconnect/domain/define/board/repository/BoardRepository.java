41c0761f99e91cfbfc11182d6d931c2d676f8efc
JeongIna <jeongina89@gmail.com>
2024-10-04 23:31:33 +0900

    feat:BoardRepository에 단일/전체 게시물 조회 기능 추가

--- backend/src/main/java/org/prgrms/devconnect/domain/define/board/repository/BoardRepository.java
+++ backend/src/main/java/org/prgrms/devconnect/domain/define/board/repository/BoardRepository.java
@@ -3,6 +3,8 @@ package org.prgrms.devconnect.domain.define.board.repository;
 
 import org.prgrms.devconnect.domain.define.board.entity.Board;
 import org.prgrms.devconnect.domain.define.board.entity.constant.BoardStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,10 +12,18 @@ import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
   @Query("SELECT b FROM Board b WHERE b.endDate< :currentDate AND b.status=:status")
   List<Board> findAllByEndDateAndStatus(@Param("currentDate") LocalDateTime currentDate,
                                         @Param("status") BoardStatus status);
+
+  // DELETED 상태가 아닌 게시글만 조회하는 메서드
+  @Query("SELECT b FROM Board b LEFT JOIN FETCH b.boardTechStacks ts LEFT JOIN FETCH ts.techStack WHERE b.status != 'DELETED'")
+  Page<Board> findAllWithTechStackByStatusNotDeleted(Pageable pageable);
+
+  @Query("SELECT b FROM Board b WHERE b.boardId = :boardId AND b.status != 'DELETED'")
+  Optional<Board> findByIdAndStatusNotDeleted(@Param("boardId") Long boardId);
 }
