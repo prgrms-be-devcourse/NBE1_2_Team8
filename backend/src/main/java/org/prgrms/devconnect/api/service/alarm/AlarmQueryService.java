9b2ceef6ddeb81ba73e423dcea6ae3a72dd32bba
eundeang <111877048+eundeang@users.noreply.github.com>
2024-10-04 14:59:17 +0900

    feat: 알림 전체 조회 메서드를 통해 예외 처리

--- backend/src/main/java/org/prgrms/devconnect/api/service/alarm/AlarmQueryService.java
+++ backend/src/main/java/org/prgrms/devconnect/api/service/alarm/AlarmQueryService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.prgrms.devconnect.api.controller.alarm.dto.response.AlarmsGetResponse;
 import org.prgrms.devconnect.api.service.member.MemberQueryService;
+import org.prgrms.devconnect.common.exception.ExceptionCode;
+import org.prgrms.devconnect.common.exception.alarm.AlarmException;
 import org.prgrms.devconnect.domain.define.alarm.entity.Alarm;
 import org.prgrms.devconnect.domain.define.alarm.repository.AlarmRepository;
 import org.prgrms.devconnect.domain.define.member.entity.Member;
@@ -19,11 +21,13 @@ public class AlarmQueryService {
 
   private final MemberQueryService memberQueryService;
 
-  public AlarmsGetResponse getAlarmsByMemberId(Long memberId) {
+  public AlarmsGetResponse getAlarmsByMemberIdOrThrow (Long memberId) {
 
     Member member = memberQueryService.getMemberByIdOrThrow(memberId);
     List<Alarm> alarms = alarmRepository.findAllByMember(member);
-
+    if (alarms.isEmpty()) {
+      throw new AlarmException(ExceptionCode.EMPTY_ALARMS);
+    }
     return AlarmsGetResponse.from(alarms);
   }
 }
