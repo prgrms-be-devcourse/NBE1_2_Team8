9506a7af2e420d87ffa11796076db16f4a8e0276
JeongIna <jeongina89@gmail.com>
2024-10-04 23:30:50 +0900

    refactor: BoardQueryService에 단일/전체 게시물 조회 기능 추가 및 리팩토링

--- backend/src/main/java/org/prgrms/devconnect/api/service/board/BoardQueryService.java
+++ backend/src/main/java/org/prgrms/devconnect/api/service/board/BoardQueryService.java
@@ -1,11 +1,14 @@
 package org.prgrms.devconnect.api.service.board;
 
 import lombok.RequiredArgsConstructor;
+import org.prgrms.devconnect.api.controller.board.dto.response.BoardResponseDto;
 import org.prgrms.devconnect.common.exception.ExceptionCode;
 import org.prgrms.devconnect.common.exception.board.BoardException;
 import org.prgrms.devconnect.domain.define.board.entity.Board;
 import org.prgrms.devconnect.domain.define.board.entity.constant.BoardStatus;
 import org.prgrms.devconnect.domain.define.board.repository.BoardRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,12 +23,18 @@ public class BoardQueryService {
   private final BoardRepository boardRepository;
 
   public Board getBoardByIdOrThrow(Long boardId) {
-    Board board= boardRepository.findById(boardId)
+    return boardRepository.findByIdAndStatusNotDeleted(boardId)
             .orElseThrow(() -> new BoardException(ExceptionCode.NOT_FOUND_BOARD));
-    if(board.isDeleted()){
-      throw new BoardException(ExceptionCode.NOT_FOUND_BOARD);
-    }
-    return board;
+  }
+
+  public BoardResponseDto getBoardById(Long boardId){
+    Board board = getBoardByIdOrThrow(boardId);
+    return BoardResponseDto.from(board);
+  }
+
+  public Page<BoardResponseDto> getAllBoards(Pageable pageable){
+    Page<Board>boards=boardRepository.findAllWithTechStackByStatusNotDeleted(pageable);
+    return boards.map(BoardResponseDto::from);
   }
 
   public List<Board> findAllByEndDateAndStatus() {
