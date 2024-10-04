fac851c63e15d4bf2d05ba6c8c2453e6ef03fc8d
JeongIna <jeongina89@gmail.com>
2024-10-04 09:39:31 +0900

    refactor: 댓글 전체 조회의 위치 이동에 따른 Service 클래스 변경

--- backend/src/main/java/org/prgrms/devconnect/api/controller/comment/CommentController.java
+++ backend/src/main/java/org/prgrms/devconnect/api/controller/comment/CommentController.java
@@ -12,6 +12,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import static org.springframework.http.HttpStatus.OK;
+
 
 @RestController
 @RequestMapping("/api/v1/comments")
@@ -28,7 +30,7 @@ public class CommentController {
 
   @GetMapping("/{boardId}")
   public ResponseEntity<Page<CommentResponseDto>> getComments(@PathVariable Long boardId, Pageable pageable) {
-    Page<CommentResponseDto>comments= commentCommandService.getCommentsByBoardId(boardId,pageable);
-    return ResponseEntity.ok(comments);
+    Page<CommentResponseDto>comments= commentQueryService.getCommentsByBoardId(boardId,pageable);
+    return ResponseEntity.status(OK).body(comments);
   }
 }
