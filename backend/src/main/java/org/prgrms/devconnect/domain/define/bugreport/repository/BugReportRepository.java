04f087804349cced6619f0b31682b8f6dbc96b54
ECON <lgyoon621@naver.com>
2024-10-04 17:46:50 +0900

    feat: memberId로 전체 조회 메서드 추가

--- backend/src/main/java/org/prgrms/devconnect/domain/define/bugreport/repository/BugReportRepository.java
+++ backend/src/main/java/org/prgrms/devconnect/domain/define/bugreport/repository/BugReportRepository.java
@@ -2,6 +2,12 @@ package org.prgrms.devconnect.domain.define.bugreport.repository;
 
 import org.prgrms.devconnect.domain.define.bugreport.entity.BugReport;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface BugReportRepository extends JpaRepository<BugReport, Long> {
+
+  List<BugReport> findAllByMember_MemberId(Long memberId);
 }
