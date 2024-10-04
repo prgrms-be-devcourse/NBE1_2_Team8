0869b9dba77ae2c63eac9b38d541cd7b47507594
ECON <lgyoon621@naver.com>
2024-10-04 11:12:39 +0900

    feat: dto 수정

--- backend/src/main/java/org/prgrms/devconnect/api/controller/chatting/dto/response/MessageResponse.java
+++ backend/src/main/java/org/prgrms/devconnect/api/controller/chatting/dto/response/MessageResponse.java
@@ -2,9 +2,11 @@ package org.prgrms.devconnect.api.controller.chatting.dto.response;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Builder;
 
 import java.time.LocalDateTime;
 
+@Builder
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record MessageResponse(
         Long messageId,
