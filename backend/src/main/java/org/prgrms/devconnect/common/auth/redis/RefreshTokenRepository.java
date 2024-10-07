fae48f2774c526f304a05122cbbb90f8226d17b7
leebs0521 <leebs0521@gmail.com>
2024-10-07 22:19:28 +0900

    feat: RefreshToken - Redis 관련 기능

---
+++ backend/src/main/java/org/prgrms/devconnect/common/auth/redis/RefreshTokenRepository.java
@@ -0,0 +1,13 @@
+package org.prgrms.devconnect.common.auth.redis;
+
+import java.util.Optional;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, Long> {
+
+  Optional<RefreshToken> findByRefreshToken(String refreshToken);
+
+  void deleteByRefreshToken(String refreshToken);
+
+}
