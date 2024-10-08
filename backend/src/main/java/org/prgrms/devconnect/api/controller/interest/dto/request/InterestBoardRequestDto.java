d347143edbc00acace5c4ee2ca84e8cd78b48f7b
eundeang <111877048+eundeang@users.noreply.github.com>
2024-10-08 21:32:23 +0900

    docs: 관심 게시글 & 관심 채용 공고 관련 api에 swagger 적용

--- backend/src/main/java/org/prgrms/devconnect/api/controller/interest/dto/request/InterestBoardRequestDto.java
+++ backend/src/main/java/org/prgrms/devconnect/api/controller/interest/dto/request/InterestBoardRequestDto.java
@@ -2,6 +2,8 @@ package org.prgrms.devconnect.api.controller.interest.dto.request;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import org.prgrms.devconnect.domain.define.board.entity.Board;
 import org.prgrms.devconnect.domain.define.interest.entity.InterestBoard;
@@ -9,15 +11,23 @@ import org.prgrms.devconnect.domain.define.member.entity.Member;
 
 @Builder
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@Schema(description = "관심 게시글 등록 요청 정보")
 public record InterestBoardRequestDto(
-    Long memberId,
-    Long boardId
+
+        @NotNull(message = "사용자 ID는 필수입니다.")
+        @Schema(description = "사용자 ID", example = "2")
+        Long memberId,
+
+        @NotNull(message = "게시물 ID는 필수입니다.")
+        @Schema(description = "게시물 ID", example = "1")
+        Long boardId
 ) {
+
 
   public InterestBoard toEntity(Member member, Board board) {
     return InterestBoard.builder()
-        .member(member)
-        .board(board)
-        .build();
+            .member(member)
+            .board(board)
+            .build();
   }
 }
