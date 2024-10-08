d94a46f6ac1430ab42334a534c9e6a8197291fd5
leebs0521 <leebs0521@gmail.com>
2024-10-08 15:58:53 +0900

    feat: 접근 토큰으로만 인증하도록 변경 - 리프레시 토큰 관련 로직은 컨트롤러를 통해(reissued)

--- backend/src/main/java/org/prgrms/devconnect/common/auth/filter/JwtTokenAuthenticationFilter.java
+++ backend/src/main/java/org/prgrms/devconnect/common/auth/filter/JwtTokenAuthenticationFilter.java
@@ -1,78 +1,95 @@
 package org.prgrms.devconnect.common.auth.filter;
 
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.security.SignatureException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.prgrms.devconnect.common.auth.JwtService;
-import org.prgrms.devconnect.common.auth.redis.RefreshTokenRepository;
 import org.prgrms.devconnect.common.exception.ExceptionCode;
-import org.prgrms.devconnect.common.exception.member.MemberException;
 import org.prgrms.devconnect.domain.define.member.entity.Member;
 import org.prgrms.devconnect.domain.define.member.repository.MemberRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+@Slf4j
 @RequiredArgsConstructor
 public class JwtTokenAuthenticationFilter extends OncePerRequestFilter {
 
   private static final String NO_CHECK_URL = "/api/v1/members/login";
-  private static final Logger logger = LoggerFactory.getLogger(JwtTokenAuthenticationFilter.class);
 
   private final JwtService jwtService;
-  private final RefreshTokenRepository refreshTokenRepository;
   private final MemberRepository memberRepository;
 
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
-      FilterChain filterChain) throws ServletException, IOException {
-    if (isLoginRequest(request)) {
-      filterChain.doFilter(request, response);
-      return;
-    }
+      FilterChain filterChain) throws IOException {
+    try {
 
-    String refreshToken = extractRefreshToken(request);
-    if (refreshToken != null) {
-      reIssueAccessToken(response, refreshToken);
-      return;
-    }
+      if (isRequestAuthorized(request)) {
+        filterChain.doFilter(request, response);
+        return;
+      }
 
-    authenticateAccessToken(request, response, filterChain);
+      authenticateAccessToken(request, response, filterChain);
+
+    } catch (ExpiredJwtException e) {
+      jwtExceptionHandler(response, ExceptionCode.JWT_TOKEN_EXPIRED);
+    } catch (UnsupportedJwtException e) {
+      jwtExceptionHandler(response, ExceptionCode.JWT_TOKEN_UNSUPPORTED);
+    } catch (MalformedJwtException e) {
+      jwtExceptionHandler(response, ExceptionCode.JWT_TOKEN_MALFORMED);
+    } catch (SignatureException e) {
+      jwtExceptionHandler(response, ExceptionCode.JWT_SIGNATURE_INVALID);
+    } catch (IllegalArgumentException e) {
+      jwtExceptionHandler(response, ExceptionCode.JWT_ARGUMENT_INVALID);
+    } catch (JwtException e) {
+      jwtExceptionHandler(response, ExceptionCode.JWT_GENERAL_ERROR);
+    } catch (Exception e) {
+      log.error("예기치 않은 오류 발생: {}", e.getMessage());
+      response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+      response.setCharacterEncoding("UTF-8");
+      response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+      response.getWriter().write("{\"error\":\"예기치 않은 오류가 발생했습니다.\"}");
+    }
+  }
+
+  private boolean isRequestAuthorized(HttpServletRequest request) {
+    Optional<String> accessToken = jwtService.extractAccessToken(request);
+
+    return accessToken.isEmpty() || isLoginRequest(request);
+  }
+
+  private void jwtExceptionHandler(HttpServletResponse response, ExceptionCode exceptionCode)
+      throws IOException {
+    log.error(">>>> [ JWT 토큰 인증 중 Error 발생 : {} ] <<<<", exceptionCode.getMessage());
+
+    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+    response.setCharacterEncoding("utf-8");
+    response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+    response.getWriter().write("{\"error\":\"" + exceptionCode.getMessage() + "\"}");
   }
 
   private boolean isLoginRequest(HttpServletRequest request) {
-    return request.getRequestURI().equals(NO_CHECK_URL);
-  }
-
-  private String extractRefreshToken(HttpServletRequest request) {
-    return jwtService.extractRefreshToken(request)
-        .filter(jwtService::isTokenValid)
-        .orElse(null);
-  }
-
-  private void reIssueAccessToken(HttpServletResponse response, String refreshToken) {
-    refreshTokenRepository.findByRefreshToken(refreshToken)
-        .ifPresentOrElse(token -> {
-          String username = token.getAuthKey();
-          Member member = memberRepository.findByEmail(username)
-              .orElseThrow(() -> new MemberException(ExceptionCode.NOT_FOUND_MEMBER));
-          String reIssuedRefreshToken = jwtService.createRefreshToken(username);
-          jwtService.sendAccessAndRefreshToken(response,
-              jwtService.createAccessToken(username),
-              reIssuedRefreshToken);
-        }, () -> logger.warn("Refresh token not found: {}", refreshToken));
+    return request.getRequestURI().equals(NO_CHECK_URL) && "POST".equalsIgnoreCase(
+        request.getMethod());
   }
 
   private void authenticateAccessToken(HttpServletRequest request,
       HttpServletResponse response,
       FilterChain filterChain) throws ServletException, IOException {
+    log.info("인증 시도");
     jwtService.extractAccessToken(request)
         .filter(jwtService::isTokenValid)
         .flatMap(jwtService::extractUsername)
@@ -85,6 +102,6 @@ public class JwtTokenAuthenticationFilter extends OncePerRequestFilter {
   private void saveAuthentication(Member member) {
     Authentication authentication = new UsernamePasswordAuthenticationToken(member, null, null);
     SecurityContextHolder.getContext().setAuthentication(authentication);
-    logger.info("인증 유저: {}", member.getEmail());
+    log.info("인증 완료 유저: {}", member.getEmail());
   }
 }
