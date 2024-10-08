8118afc539c95d0b4931dc3fabdeba10f83d040b
ECON <lgyoon621@naver.com>
2024-10-08 12:03:53 +0900

    fix: @Param 추가

--- backend/src/main/java/org/prgrms/devconnect/domain/define/member/repository/MemberRepository.java
+++ backend/src/main/java/org/prgrms/devconnect/domain/define/member/repository/MemberRepository.java
@@ -1,9 +1,11 @@
 package org.prgrms.devconnect.domain.define.member.repository;
 
 import java.util.Optional;
+
 import org.prgrms.devconnect.domain.define.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -12,7 +14,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
   Optional<Member> findByEmail(String email);
 
   @Query("SELECT m FROM Member m LEFT JOIN FETCH m.memberTechStacks mts LEFT JOIN FETCH mts.techStack WHERE m.memberId = :memberId")
-  Optional<Member> findByMemberIdWithTechStack(Long memberId);
+  Optional<Member> findByMemberIdWithTechStack(@Param("memberId") Long memberId);
 
   boolean existsByEmail(String email);
 }
