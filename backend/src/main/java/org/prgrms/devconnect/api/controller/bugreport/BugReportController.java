9f47d0a0f69f3d848470b624f3ae58a5f58a8330
leebs0521 <leebs0521@gmail.com>
2024-10-08 21:34:16 +0900

    feat: BugReport 관련 시큐리티 적용

--- backend/src/main/java/org/prgrms/devconnect/api/controller/bugreport/BugReportController.java
+++ backend/src/main/java/org/prgrms/devconnect/api/controller/bugreport/BugReportController.java
@@ -1,5 +1,9 @@
 package org.prgrms.devconnect.api.controller.bugreport;
 
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
+import static org.springframework.http.HttpStatus.OK;
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -7,6 +11,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.prgrms.devconnect.api.controller.bugreport.dto.request.BugReportRequest;
 import org.prgrms.devconnect.api.controller.bugreport.dto.request.BugReportUpdateRequest;
@@ -14,16 +19,21 @@ import org.prgrms.devconnect.api.controller.bugreport.dto.response.BugReportResp
 import org.prgrms.devconnect.api.service.bugreport.BugReportCommendService;
 import org.prgrms.devconnect.api.service.bugreport.BugReportQueryService;
 import org.prgrms.devconnect.domain.define.bugreport.entity.BugReport;
+import org.prgrms.devconnect.domain.define.member.entity.Member;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-import static org.springframework.http.HttpStatus.*;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/bugreport")
+@RequestMapping("/api/v1/bug-report")
 @Tag(name = "버그리포트 API", description = "버그 관련 신고를 하는 게시판")
 public class BugReportController {
 
@@ -34,36 +44,39 @@ public class BugReportController {
 
   @Operation(summary = "버그 리포트 생성", description = "새로운 버그 리포트를 생성합니다.")
   @ApiResponse(responseCode = "201", description = "버그 리포트가 성공적으로 생성되었습니다.",
-          content = @Content(schema = @Schema(implementation = BugReport.class)))
+      content = @Content(schema = @Schema(implementation = BugReport.class)))
   @PostMapping
-  public ResponseEntity<BugReportResponse> createBugReport(@RequestBody @Valid BugReportRequest dto) {
-    BugReportResponse response = bugReportCommendService.createBugReport(dto.memberId(),
-            dto.url(),
-            dto.content(),
-            dto.bugType());
+  public ResponseEntity<BugReportResponse> createBugReport(@RequestBody @Valid BugReportRequest dto,
+      @AuthenticationPrincipal Member member) {
+    BugReportResponse response = bugReportCommendService.createBugReport(member.getMemberId(),
+        dto.url(),
+        dto.content(),
+        dto.bugType());
 
     return ResponseEntity.status(CREATED).body(response);
   }
 
   @Operation(summary = "모든 버그 리포트 조회", description = "모든 버그 리포트를 조회합니다.")
   @ApiResponse(responseCode = "200", description = "버그 리포트 목록을 성공적으로 반환합니다.",
-          content = @Content(schema = @Schema(implementation = BugReportResponse.class)))
+      content = @Content(schema = @Schema(implementation = BugReportResponse.class)))
   @ApiResponse(responseCode = "204", description = "버그 리포트가 없습니다.")
   @GetMapping
   public ResponseEntity<List<BugReportResponse>> getAllBugReports() {
     List<BugReportResponse> results = bugReportQueryService.getAllBugReport();
 
-    if (results.isEmpty())
+    if (results.isEmpty()) {
       return ResponseEntity.status(NO_CONTENT).build();
+    }
 
     return ResponseEntity.status(OK).body(results);
   }
 
   @Operation(summary = "특정 버그 리포트 조회", description = "버그 리포트 ID로 버그 리포트의 상세 정보를 조회합니다.")
   @ApiResponse(responseCode = "200", description = "버그 리포트 상세 정보를 성공적으로 반환합니다.",
-          content = @Content(schema = @Schema(implementation = BugReport.class)))
+      content = @Content(schema = @Schema(implementation = BugReport.class)))
   @GetMapping("/{reportId}")
-  public ResponseEntity<BugReportResponse> getBugReportById(@PathVariable("reportId") Long reportId) {
+  public ResponseEntity<BugReportResponse> getBugReportById(
+      @PathVariable("reportId") Long reportId) {
     BugReport bugReport = bugReportQueryService.getBugReport(reportId);
     BugReportResponse response = BugReportResponse.from(bugReport);
     return ResponseEntity.status(OK).body(response);
@@ -74,8 +87,9 @@ public class BugReportController {
   @Parameter(name = "reportId", description = "수정할 버그 리포트의 ID", required = true)
   @PutMapping("/{reportId}")
   public ResponseEntity<BugReportResponse> updateBugReport(@PathVariable("reportId") Long reportId,
-                                                   @RequestBody @Valid BugReportUpdateRequest dto) {
-    BugReportResponse response = bugReportCommendService.updateBugReport(reportId, dto.url(), dto.content(), dto.bugType());
+      @RequestBody @Valid BugReportUpdateRequest dto) {
+    BugReportResponse response = bugReportCommendService.updateBugReport(reportId, dto.url(),
+        dto.content(), dto.bugType());
 
     return ResponseEntity.status(OK).body(response);
   }
@@ -92,15 +106,17 @@ public class BugReportController {
 
   @Operation(summary = "특정 사용자의 모든 버그 리포트 조회", description = "특정 사용자의 모든 버그 리포트를 조회합니다.")
   @ApiResponse(responseCode = "200", description = "해당 사용자의 버그 리포트 목록을 성공적으로 반환합니다.",
-          content = @Content(schema = @Schema(implementation = BugReportResponse.class)))
+      content = @Content(schema = @Schema(implementation = BugReportResponse.class)))
   @ApiResponse(responseCode = "204", description = "해당 사용자의 버그 리포트가 없습니다.")
-  @Parameter(name = "memberId", description = "조회할 사용자의 ID", required = true)
-  @GetMapping("/member/{memberId}")
-  public ResponseEntity<List<BugReportResponse>> getAllBugReportsByMemberId(@PathVariable("memberId") Long memberId) {
-    List<BugReportResponse> results = bugReportQueryService.getAllBugReportByMemberId(memberId);
+  @GetMapping("/member")
+  public ResponseEntity<List<BugReportResponse>> getAllBugReportsByMemberId(
+      @AuthenticationPrincipal Member member) {
+    List<BugReportResponse> results = bugReportQueryService.getAllBugReportByMemberId(
+        member.getMemberId());
 
-    if (results.isEmpty())
+    if (results.isEmpty()) {
       return ResponseEntity.status(NO_CONTENT).build();
+    }
 
     return ResponseEntity.status(OK).body(results);
   }
--- backend/src/main/java/org/prgrms/devconnect/domain/config/SecurityConfig.java
+++ backend/src/main/java/org/prgrms/devconnect/domain/config/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.prgrms.devconnect.common.auth.redis.RefreshTokenRepository;
 import org.prgrms.devconnect.domain.define.member.repository.MemberRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -36,7 +37,6 @@ public class SecurityConfig {
   private final JwtService jwtService;
   private final MemberRepository memberRepository;
   private final CustomerMemberDetailsService customerMemberDetailsService;
-  private final RefreshTokenRepository refreshTokenRepository;
   private final ObjectMapper objectMapper;
 
   @Bean
@@ -54,8 +54,18 @@ public class SecurityConfig {
             authorize -> authorize
                 .requestMatchers("/", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                 .requestMatchers("/static/**", "/templates/**").permitAll()
-                .requestMatchers("/api/v1/members/login", "/api/v1/members/signup", "/api/v1/members/reissue").permitAll()
+                // 멤버
+                .requestMatchers("/api/v1/members/login", "/api/v1/members/signup",
+                    "/api/v1/members/reissue").permitAll()
                 .requestMatchers("/api/v1/members/logout", "/api/v1/members").authenticated()
+                // 알림
+                .requestMatchers("/api/v1/alarms/**").authenticated()
+                // 게시판
+                .requestMatchers(HttpMethod.GET, "/api/v1/boards/**").permitAll()
+                .requestMatchers(HttpMethod.GET, "/api/v1/boards/interests").authenticated()
+                .requestMatchers("/api/v1/boards/**").authenticated()
+                // 버그 리포트
+                .requestMatchers("/api/v1/bug-report/**").authenticated()
                 .anyRequest().permitAll()
 
         );
