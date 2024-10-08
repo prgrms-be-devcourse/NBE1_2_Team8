94ee1296ab6a1989f247ad9d7866392c4d4188e1
ECON <lgyoon621@naver.com>
2024-10-08 16:25:25 +0900

    feat: deleteByIds 메서드 추가

--- backend/src/main/java/org/prgrms/devconnect/domain/define/member/repository/MemberTechStackMappingRepository.java
+++ backend/src/main/java/org/prgrms/devconnect/domain/define/member/repository/MemberTechStackMappingRepository.java
@@ -2,6 +2,9 @@ package org.prgrms.devconnect.domain.define.member.repository;
 
 import org.prgrms.devconnect.domain.define.member.entity.MemberTechStackMapping;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -9,4 +12,7 @@ public interface MemberTechStackMappingRepository extends JpaRepository<MemberTe
 
   List<MemberTechStackMapping> findAllByMember_MemberIdAndTechStack_TechStackIdIn(Long memberId, List<Long> techStackIds);
 
+  @Modifying
+  @Query("DELETE FROM MemberTechStackMapping m WHERE m.id IN :ids")
+  void deleteAllByIds(@Param("ids") List<Long> ids);
 }
