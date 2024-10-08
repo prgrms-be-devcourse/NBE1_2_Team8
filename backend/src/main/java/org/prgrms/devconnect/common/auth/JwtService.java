7e60301083e6e812f468cd088e7d4a8ed39267ac
leebs0521 <leebs0521@gmail.com>
2024-10-08 15:59:49 +0900

    refactor: 불필요한 로직 제거 및 일부 기능 변경 - 리프레시 토큰은 쿠키로 저장

--- backend/src/main/java/org/prgrms/devconnect/common/auth/JwtService.java
+++ backend/src/main/java/org/prgrms/devconnect/common/auth/JwtService.java
@@ -3,6 +3,7 @@ package org.prgrms.devconnect.common.auth;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.nio.charset.StandardCharsets;
@@ -11,12 +12,19 @@ import java.util.Optional;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.prgrms.devconnect.common.auth.redis.RefreshToken;
 import org.prgrms.devconnect.common.auth.redis.RefreshTokenRepository;
+import org.prgrms.devconnect.common.exception.ExceptionCode;
+import org.prgrms.devconnect.common.exception.member.MemberException;
+import org.prgrms.devconnect.common.exception.refresh.RefreshTokenException;
+import org.prgrms.devconnect.domain.define.member.entity.Member;
+import org.prgrms.devconnect.domain.define.member.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class JwtService {
 
@@ -37,6 +45,7 @@ public class JwtService {
 
   private static final String BEARER = "Bearer ";
   private final RefreshTokenRepository refreshTokenRepository;
+  private final MemberRepository memberRepository;
 
 
   private SecretKey getSecretKey() {
@@ -75,13 +84,7 @@ public class JwtService {
       String refreshToken) {
     response.setStatus(HttpServletResponse.SC_OK);
     setAccessTokenHeader(response, accessToken);
-    setRefreshTokenHeader(response, refreshToken);
-  }
-
-  public Optional<String> extractRefreshToken(HttpServletRequest request) {
-    return Optional.ofNullable(request.getHeader(refreshHeader))
-        .filter(refreshToken -> refreshToken.startsWith(BEARER))
-        .map(refreshToken -> refreshToken.replace(BEARER, ""));
+    setRefreshTokenCookie(response, refreshToken);
   }
 
   public Optional<String> extractAccessToken(HttpServletRequest request) {
@@ -91,36 +94,49 @@ public class JwtService {
   }
 
   public Optional<String> extractUsername(String accessToken) {
-    try {
-      return Optional.ofNullable(getClaimsJws(accessToken).getPayload()
-          .get("username", String.class));
-    } catch (Exception e) {
-      return Optional.empty();
-    }
+    return Optional.ofNullable(getClaimsJws(accessToken).getPayload()
+        .get("username", String.class));
   }
 
   private void setAccessTokenHeader(HttpServletResponse response, String accessToken) {
     response.setHeader(accessHeader, BEARER + accessToken);
   }
 
-  private void setRefreshTokenHeader(HttpServletResponse response, String refreshToken) {
-    response.setHeader(refreshHeader, BEARER + refreshToken);
+  private void setRefreshTokenCookie(HttpServletResponse response, String refreshToken) {
+    Cookie cookie = new Cookie(refreshHeader, refreshToken);
+    cookie.setHttpOnly(true);
+    cookie.setSecure(true);
+    cookie.setPath("/");
+    cookie.setMaxAge((int) (refreshTokenExpirationPeriod / 1000));
+    response.addCookie(cookie);
   }
 
   public boolean isTokenValid(String token) {
-    try {
-      getClaimsJws(token);
-      return true;
-    } catch (Exception e) {
-      return false;
-    }
+    getClaimsJws(token);
+    return true;
   }
 
-  public Jws<Claims> getClaimsJws(String token) {
+  private Jws<Claims> getClaimsJws(String token) {
     return Jwts.parser()
         .verifyWith(getSecretKey())
         .build()
         .parseSignedClaims(token);
   }
 
+  public void reIssueAccessToken(HttpServletResponse response, String refreshToken) {
+    log.info("Access Token 재발급 시도: {}", refreshToken);
+    refreshTokenRepository.findByRefreshToken(refreshToken)
+        .ifPresentOrElse(token -> {
+          String username = token.getAuthKey();
+          Member member = memberRepository.findByEmail(username)
+              .orElseThrow(() -> new MemberException(ExceptionCode.NOT_FOUND_MEMBER));
+          String reIssueAccessToken = this.createAccessToken(username);
+          this.setAccessTokenHeader(response, reIssueAccessToken);
+
+          log.info("Access Token 재발급 성공");
+        }, () -> {
+          log.warn("Access Token 발급 실패");
+          throw new RefreshTokenException(ExceptionCode.NOT_FOUND_REFRESH_TOKEN);
+        });
+  }
 }
