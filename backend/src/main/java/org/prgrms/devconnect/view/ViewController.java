bb06a2def9d51c3ccadce1ac08a6f21ecc11e7ef
ECON <lgyoon621@naver.com>
2024-10-09 21:48:31 +0900

    feat: index.html 각 상세 조회 페이지 이동 기능 추가

--- backend/src/main/java/org/prgrms/devconnect/view/ViewController.java
+++ backend/src/main/java/org/prgrms/devconnect/view/ViewController.java
@@ -2,6 +2,7 @@ package org.prgrms.devconnect.view;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,5 +17,17 @@ public class ViewController {
   @GetMapping("login")
   public String login() {
     return "login";
+  }
+
+  @GetMapping("/boards/{boardId}")
+  public String getBoardDetailPage(@PathVariable("boardId") Long boardId) {
+    // 페이지 이동만 처리
+    return "board-detail";  // board-detail.html 페이지 반환
+  }
+
+  @GetMapping("/job-posts/{jobPostId}")
+  public String getJobPostDetailPage(@PathVariable("jobPostId") Long jobPostId) {
+    // 페이지 이동만 처리
+    return "job-detail"; // job-detail.html 반환
   }
 }
